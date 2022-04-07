463cbdeeed59baaeeab5a6e7752080f7758702a8
Otávio Augusto <62617193+GutsGT@users.noreply.github.com>
2022-04-07 11:06:44 -0300

    Update main.cpp

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -4,9 +4,15 @@ using std::cout;
 
 const int SIZE = 5; // input size.
 
-int main(void)
-{
-    // TODO: Adicione aqui seu código.
-
-    return 0;
+int main(void){
+  // TODO: Adicione aqui seu código.
+  int x;
+  int qtd = 0;
+  for(int i = 0; i < 5; i++){
+    cin>>x;
+    if(x < 0) qtd++;
+  }
+  cout<<"A quantidade de números negativos é: "<<qtd<<std::endl;
+  
+  return 0;
 }
