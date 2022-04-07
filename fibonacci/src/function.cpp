bf71629be77606853bfae996e365ec2dcf27dcfd
Otávio Augusto <62617193+GutsGT@users.noreply.github.com>
2022-04-07 11:06:02 -0300

    Update function.cpp

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,33 @@
+#include <iostream>
 #include "function.h"
 
-std::vector<unsigned int> fib_below_n( unsigned int n )
-{
+using namespace std;
+
+std::vector<unsigned int> fib_below_n( unsigned int n ){
     // TODO: adicione o seu código aqui.
 
+	vector<unsigned int> values;
+
+	while(values.size() == 0 || values.at(values.size()-1) < n){
+		if(values.size() < 2){
+			if(n >= 2){
+				values.insert(values.end(), 1);
+				cout<<"        ";	
+			}else{
+				break;
+			}
+		}else{			
+			if((values.at(values.size()-2) + values.at(values.size()-1)) < n){
+				cout<<values.at(values.size()-1)<<" < "<<n<<" - ";
+				values.insert(values.end(), (values.at(values.size()-2) + values.at(values.size()-1)));		
+			}else{
+				break;
+			}
+		}	
+		cout<<values.at(values.size()-1)<<endl;
+	}
+	cout<<endl<<endl<<endl;
+	
     // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    return std::vector<unsigned int>{values};
 }
